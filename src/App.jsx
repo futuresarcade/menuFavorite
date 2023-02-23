c6e4ce862ee52eb219bd2b9d8fba7bc39ccc0aa8
Thorvald <futuresarcade@gmail.com>
2023-02-23 18:45:08 -0500

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import Menu from './Menu'
+import Favorite from './Favorite'
+import Toggler from './Toggler'
+
+function App() {
+    return (
+        <div>
+            <Toggler render={({ on, toggle }) => {
+                return (
+                    <Menu on={on} toggle={toggle} />
+                )
+            }}
+            />
+            <hr />
+            <Favorite />
+        </div>
+    )
+
+
+}
+
+export default App
